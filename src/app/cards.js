d184b9c2d824dd560e3874779d7174373eadfe99
c.sipiran07@gmail.com <carlos0718>
2024-08-25 16:31:33 -0300

    modifications of styles in card container

--- src/app/cards.js
+++ src/app/cards.js
@@ -6,11 +6,10 @@ export function createCards() {
 	getProducts().then((data) => {
 		data.map((prod) => {
 			let card = `<div class="col">
-                           <div class="card">
-                           <img src=${prod.image} class="card-img-top" alt=${prod.title}>
-                           <div class="card-body">
+                           <div class="card" style="height: 500px">
+                           <img src=${prod.image} class="card-img-top img-fluid object-fit-contain h-75" alt=${prod.title}>
+                           <div class="card-body text-center">
                                <h5 class="card-title">${prod.title}</h5>
-                               <p class="card-text">${prod.description}</p>
                            </div>
                            </div>
                        </div>`;
